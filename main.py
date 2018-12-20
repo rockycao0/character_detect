3e75ed084436024719d6525ac290b48004a18e5c
rockycao <rockycao0@163.com>
2018-12-20 11:08:46 +0800

    fin

--- main.py
+++ main.py
@@ -1,104 +1,56 @@
 # coding in utf-8
 # by rockycao
-import math
+
 import wave
+
+import matplotlib.pyplot as plt
 import numpy as np
-import pylab as pl
-import volume as vp
+from sklearn.cluster import KMeans
 
-
-def ZeroCR(waveData, frameSize, overLap):
-    wlen = len(waveData)
-    step = frameSize - overLap
-    frameNum = math.ceil(wlen / step)
-    zcr = np.zeros((frameNum, 1))
-    for i in range(frameNum):
-        curFrame = waveData[np.arange(i * step, min(i * step + frameSize, wlen))]
-        # To avoid DC bias, usually we need to perform mean subtraction on each frame
-        # ref: http://neural.cs.nthu.edu.tw/jang/books/audiosignalprocessing/basicFeatureZeroCrossingRate.asp
-        curFrame = curFrame - np.mean(curFrame)  # zero-justified
-        zcr[i] = sum(curFrame[0:-1] * curFrame[1::] <= 0)
-    return zcr
-
-
-# ============ test the algorithm =============
-# read wave file and get parameters.
-fw = wave.open('sample.wav', 'rb')
+times = 11
+num = 10
+# todo:use librosa
+fw = wave.open('1_2.wav', 'rb')
 params = fw.getparams()
-# print(params)
 nchannels, sampwidth, framerate, nframes = params[:4]
 str_data = fw.readframes(nframes)
-wave_data = np.fromstring(str_data, dtype=np.short)
-wave_data = wave_data[int(framerate * 60 * nchannels):framerate * 65 * nchannels]
-
-wave_data.shape = -1, 1
 fw.close()
-frameSize = 256
-overLap = 128
+wave_data = np.fromstring(str_data, dtype=np.short)
+wave_data.shape = -1, 1
+wave_data = wave_data.T
+time = np.arange(0, nframes) * (1.0 / framerate / nchannels)
+end_time = time[-1]
+norm_time = time * framerate * times  # k-means时正则化
+wave = np.dstack((wave_data, norm_time)).reshape(-1, 2)
+wave = np.array(wave)
 
-volume11 = vp.calVolume(wave_data, frameSize, overLap)
-zcr = ZeroCR(wave_data, frameSize, overLap)
-i = 0
-voice = [0, 0]
-flag_zcr = 0
-flag_er = 0
-split = []
-zcr_mean = np.mean(zcr)
-er_mean = np.mean(volume11)
-time = np.arange(0, len(wave_data)) * (1.0 / framerate / nchannels)
-time2 = np.arange(0, len(zcr)) * (len(wave_data) / len(zcr) / framerate / nchannels)
-rate = 0.5
-for i in range(len(zcr)):
-    if zcr[i] >= zcr_mean and flag_er + flag_zcr == 0 and volume11[i] > rate * er_mean:
-        flag_zcr = 1
-        voice[0] = i
-        print('zcr up')
-    elif volume11[i] > rate * er_mean and flag_er + flag_zcr == 0:
-        flag_er = 1
-        voice[0] = i
-        print('er up')
-    elif flag_zcr == 1 and zcr[i] < rate * zcr_mean:
-        flag_zcr = 0
-        voice[1] = i
-        if voice[1] - voice[0] > 10:
-            split.append([voice[0], voice[1]])
-        print('zcr down')
-    elif flag_er == 1 and volume11[i] < rate * er_mean:
-        flag_er = 0
-        voice[1] = i
-        if voice[1]-voice[0]>10:
-            split.append([voice[0], voice[1]])
-        print('er down')
+'''wave_split = []
+for frame in wave:
+    if frame[0] > 0.1 * framerate:
+        wave_split.append(frame)
+wave_split = np.array(wave_split)
+'''
+y_pred = KMeans(n_clusters=10).fit_predict(wave)
+time = np.delete(wave, 0, axis=1)
+time = time / framerate / times
+plt.scatter(wave[:, 1], wave[:, 0] / framerate / times, c=y_pred)
+plt.savefig("sample")
+plt.show()
 
+time_string = np.dstack((time, y_pred.reshape(-1, 1))).reshape(-1, 2)
+a = np.full((num, 1), end_time)
+b = np.zeros((num, 1))
+a = np.dstack((a, b)).reshape(-1, 2)
+
+for i in time_string:
+    num = int(i[1])
+    t = i[0]
+    if t < a[num][0]:
+        a[num][0] = t
+    elif t > a[num][1]:
+        a[num][1] = t
+a = a[a[:, 0].argsort()]
 f = open("span.txt", 'w')
-num = 0
-for span in split:
-    num += 1
-    f.write("{0}--{1}".format(span[0], span[1]))
-    f.write('\n')
+for i in a:
+    f.write("{}--{}\n".format(i[0], i[1]))
 f.close()
-
-for number in range(num):
-    slice = wave.open(r"D:\github\audio\slice\%s.wav" % number, "wb")
-    # 配置声道数、量化位数和取样频率
-    slice.setnchannels(nchannels)
-    slice.setsampwidth(sampwidth)
-    slice.setframerate(framerate)
-    # 将wav_data转换为二进制数据写入文件
-    slice.writeframes(wave_data.tostring())
-    slice.close()
-
-pl.subplot(311)
-pl.plot(time, wave_data)
-pl.ylabel("wave")
-pl.subplot(312)
-pl.plot(time2, zcr)
-pl.ylabel("ZCR")
-pl.xlabel("time (seconds)")
-pl.subplot(313)
-pl.plot(time2, volume11)
-pl.ylabel("volume")
-pl.xlabel("time (seconds)")
-sample = pl.gcf()
-sample.savefig("sample")
-pl.show()
